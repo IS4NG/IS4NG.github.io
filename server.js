95e526668107eb0deef796ea10ebf2c9e9e8eba6
iminun <hehe427@naver.com>
2022-04-06 16:32:15 +0900

    'merge'

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
-app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
 const bodyParser= require('body-parser');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
