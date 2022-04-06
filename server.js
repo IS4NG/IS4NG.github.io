51ed01992cb9d1153f57bc452b5e7b117d81dd02
iminun <hehe427@naver.com>
2022-04-06 15:39:37 +0900

    'dddd'

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 const bodyParser= require('body-parser');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
